272b3a4b43e540ab670b919dc48c66da43fc9b11
ewilliams-zoot <ezekiel.williams@zootweb.com>
2021-11-14 21:46:02 -0700

    Add insert capability in lists
    Simplified to a single type of drag object for now, so the drag data is hard-coded. Also testing out drag enter transition animations

--- src/components/flow_objects/ExpressionStatement.vue
+++ src/components/flow_objects/ExpressionStatement.vue
@@ -2,16 +2,20 @@
 import CallExpression from './CallExpression.vue';
 import commonProps from '../../common_ast_props';
 import { useStore } from 'vuex';
+import { ref } from 'vue';
 
 const props = defineProps({
     ...commonProps,
     expression: Object
 });
 
+let isOverInsertSpot = ref(false);
+
 const store = useStore();
 
 function dragOverHandler(event) {
     // do something like change cursor
+    event.stopPropagation();
 }
 
 /**
@@ -39,12 +43,45 @@ function dropHandler(event) {
         }
     }});
 }
+
+function insertDragOverHandler(event) {
+    // something
+}
+function insertDragEnter(event) {
+    isOverInsertSpot.value = true;
+}
+function insertDragLeave(event) {
+    isOverInsertSpot.value = false;
+}
+
+function insertDropHandler(event) {
+    event.stopPropagation();
+    isOverInsertSpot.value = false;
+
+    const baseLocation = `${props.location}`;
+    store.commit('insertNode', {
+        insertAfterLocation: baseLocation,
+        newNode: {
+            type: "ExpressionStatement",
+            expression: null
+        }
+    });
+
+}
 </script>
 
 <template>
     <div @dragover.prevent="dragOverHandler" @drop.prevent="dropHandler" class="expression-container">
         <component v-if="expression != null" :is="expression.type" v-bind="expression"></component>
         <p class="dull-text" v-if="expression == null">Drag an action here</p>
+    </div>
+    <div
+        @dragover.prevent="insertDragOverHandler"
+        @dragenter.prevent="insertDragEnter"
+        @dragleave.prevent="insertDragLeave"
+        @drop.prevent="insertDropHandler"
+        class="line-down-box"
+        :class="{ 'insert-drag-over': isOverInsertSpot }">
     </div>
 </template>
 
@@ -60,5 +97,18 @@ export default {
 .expression-container {
     padding: 35px;
     border: 1px dashed black;
+}
+
+.line-down-box {
+    margin-left: 20px;
+    border-left: 1px dashed black;
+    height: 30px;
+    /* transition: height 0.5s ease-out; */
+}
+
+.insert-drag-over {
+    border-left: 2px dashed green;
+    transition: height 0.3s ease-out;
+    height: 45px;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -19,15 +19,50 @@ const store = createStore({
             parent[toEnd].location = toLocation;
             parent[fromEnd].location = fromLocation;
         },
+        /**
+         * 
+         * @param {Object} state 
+         * @param {Object} param1
+         * @param {string} param1.toLocation
+         * @param {Object} param1.node
+         */
         addNode(state, { toLocation, node }) {
             const parent = fetchParentUsingLocation(state.ast, toLocation);
             const placeInParent = getLastItemInLocation(toLocation);
             parent[placeInParent] = node;
-            console.log(toLocation);
+        },
+        /**
+         * 
+         * @param {Object} state
+         * @param {Object} param1 
+         * @param {string} param1.insertAfterLocation
+         * @param {Object} param1.newNode
+         */
+        insertNode(state, { insertAfterLocation, newNode }) {
+            const parent = fetchParentUsingLocation(state.ast, insertAfterLocation);
+            const insertAfterIndex = parseInt(getLastItemInLocation(insertAfterLocation));
+
+            // Modify location properties before splicing in new element
+            for (let i = insertAfterIndex + 1; i < parent.length; i += 1) {
+                const node = parent[i];
+                node.location = node.location.replace(/\..*$/, `.${i + 1}`);
+            }
+
+            // Add new node into the tree
+            const parentLocation = insertAfterLocation.replace(/^(.*)\..*$/, "$1");
+            newNode.location = `${parentLocation}.${insertAfterIndex + 1}`;
+            parent.splice(insertAfterIndex + 1, 0, newNode);
         }
     }
 });
 
+/**
+ * Traverses the tree using the given location and returns the
+ * parent of the node at given location
+ * @param {Object} ast - Root of the JSON syntax tree
+ * @param {string} location - Dot-delimited path to a node in the tree
+ * @returns {Object}
+ */
 function fetchParentUsingLocation(ast, location) {
     const nodePaths = location.split('.');
 
@@ -40,6 +75,11 @@ function fetchParentUsingLocation(ast, location) {
     return currentNode;
 }
 
+/**
+ * 
+ * @param {string} location 
+ * @returns {string}
+ */
 function getLastItemInLocation(location) {
     const locations = location.split('.');
     return locations[locations.length - 1];
