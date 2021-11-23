7ffb65ba4eb70c70b12ec7bf1e76e3910e77b015
ewilliams-zoot <ezekiel.williams@zootweb.com>
2021-11-22 22:38:21 -0700

    Add utility pane and first pass at StringUtil

--- src/components/container_components/side_nav/SideNav.vue
+++ src/components/container_components/side_nav/SideNav.vue
@@ -1,14 +1,37 @@
 <script setup>
 import FlowObjectSource from './FlowObjectSource.vue';
+import UtilityObject from '../../utilities/UtilityObject.vue';
 
 const doActionData = {
     prop1: "hello",
     type: "ExpressionStatement"
 };
+
+const stringUtilData = {
+    type: "CallExpression",
+    callee: {
+        type: "MemberExpression",
+        object: {
+            type: "Identifier",
+            name: "StringUtil"
+        },
+        property: {
+            type: "Identifier",
+            name: "concat"
+        }
+    },
+    arguments: []
+}
 </script>
 <template>
     <div class="side-nav-wrapper">
-        <FlowObjectSource :dragData="doActionData">Drag Me Tester</FlowObjectSource>
+        <div class="project-structure-pane">
+            <FlowObjectSource :dragData="doActionData">Drag Me Tester</FlowObjectSource>
+        </div>
+
+        <div class="utility-pane">
+            <UtilityObject :dragData="stringUtilData">String Util</UtilityObject>
+        </div>
     </div>
     
 </template>
@@ -19,5 +42,17 @@ const doActionData = {
     max-width: 500px;
     height: 100%;
     background: #ddd;
+    display: flex;
+    flex-direction: column;
+}
+
+.project-structure-pane {
+    flex: 1;
+}
+
+.utility-pane {
+    min-height: 200px;
+    overflow: auto;
+    border-top: 2px solid #888;
 }
 </style>
---
+++ src/components/utilities/UtilityObject.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+    dragData: Object
+});
+
+/**
+ * @param {DragEvent} event
+ */
+function dragStartHandler(event) {
+    event.dataTransfer.setData("text/json", JSON.stringify(props.dragData));
+    event.dataTransfer.dropEffect = "copy";
+}
+</script>
+
+<template>
+    <div class="utility-object-tile" draggable="true" @dragstart="dragStartHandler">
+        <p><slot></slot></p>
+    </div>
+</template>
