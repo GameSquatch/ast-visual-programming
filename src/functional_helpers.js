230b6395e6daa527a999f468d4f8fec368090826
ewilliams-zoot <ezekiel.williams@zootweb.com>
2021-12-05 21:43:45 -0700

    Add functional helpers

---
+++ src/functional_helpers.js
@@ -0,0 +1,18 @@
+
+const compose = (...fns) => (...args) => {
+    return fns.reduceRight((acc, fn) => [fn(...acc)], args)[0];
+};
+
+const curry = (fn) => {
+    let arity = fn.length;
+
+    return function $curry(...args) {
+        if (args.length < arity) {
+            return $curry.bind(null, ...args);
+        }
+
+        return fn.apply(null, args);
+    };
+};
+
+const map = curry((fn, xs) => xs.map(fn));
