82b01cab91c339324887740e9e0dfbd9c34bc5f2
GameSquatch <broncoscorpio@gmail.com>
2021-12-08 22:44:31 -0700

    Remove remnants of location-based node mutations

--- src/store/index.js
+++ src/store/index.js
@@ -7,37 +7,6 @@ const store = createStore({
         ast: mockData
     },
     mutations: {
-        swapNodes(state, { fromLocation, toLocation }) {
-            const parent = fetchParentUsingLocation(state.ast, fromLocation);
-            const fromPaths = fromLocation.split('.');
-            const toPaths = toLocation.split('.');
-            const fromEnd = fromPaths[fromPaths.length - 1];
-            const toEnd = toPaths[toPaths.length - 1];
-
-            const tempRef = parent[toEnd];
-            parent[toEnd] = parent[fromEnd];
-            parent[fromEnd] = tempRef;
-            parent[toEnd].location = toLocation;
-            parent[fromEnd].location = fromLocation;
-        },
-        /**
-         * 
-         * @param {Object} state 
-         * @param {Object} param1
-         * @param {string} param1.location
-         * @param {Object} param1.node
-         */
-        addNode(state, { location, node }) {
-            const parent = fetchParentUsingLocation(state.ast, location);
-            const placeInParent = getLastItemInLocation(location);
-            // Expression statements are always the outer wrappers to other expressions. I.e.
-            // you will never find and expression statement inside another at any level.
-            if (node.type == "ExpressionStatement" && parent.type == "ExpressionStatement") {
-                return;
-            }
-            node.location = location;
-            parent[placeInParent] = node;
-        },
         /**
          * 
          * @param {Object} state
@@ -55,34 +24,5 @@ const store = createStore({
         }
     }
 });
-
-/**
- * Traverses the tree using the given location and returns the
- * parent of the node at given location
- * @param {Object} ast - Root of the JSON syntax tree
- * @param {string} location - Dot-delimited path to a node in the tree
- * @returns {Object}
- */
-function fetchParentUsingLocation(ast, location) {
-    const nodePaths = location.split('.');
-
-    let currentNode = ast.main;
-    let i = 0;
-    for (i; i < nodePaths.length - 1; ++i) {
-        currentNode = currentNode[nodePaths[i]];
-    }
-
-    return currentNode;
-}
-
-/**
- * 
- * @param {string} location 
- * @returns {string}
- */
-function getLastItemInLocation(location) {
-    const locations = location.split('.');
-    return locations[locations.length - 1];
-}
 
 export default store;
