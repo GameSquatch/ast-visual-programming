f2b000771510ca7cdf9aafa6098130c48b024425
GameSquatch <broncoscorpio@gmail.com>
2022-06-23 20:33:59 -0600

    Rearrange top level components
    The idea is that I don't need a new component unless it has state or will be re-used

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,19 @@
 <script>
-    import AppWrapper from './components/container_components/AppWrapper.svelte';
+    import SideNav from './components/container_components/side_nav/SideNav.svelte';
+    import FlowController from './components/container_components/FlowController.svelte';
     import Header from './components/Header.svelte';
 </script>
 
 <Header />
-<AppWrapper />
+<div class="w100 content-wrapper">
+    <SideNav />
+    <FlowController />
+</div>
+    
+<style>
+    .content-wrapper {
+        flex: 1;
+        display: flex;
+        overflow: hidden;
+    }
+</style>
--- src/components/container_components/side_nav/SideNav.svelte
+++ src/components/container_components/side_nav/SideNav.svelte
@@ -1,6 +1,6 @@
 <script>
     import FlowObjectSource from './FlowObjectSource.svelte';
-    import { doActionDataDrag, stringUtilDataDrag } from '../../../drag_start_data_creators.js';
+    import { stringUtilDataDrag } from '../../../drag_start_data_creators.js';
     import { fileTree, openFunction } from '../../../store/stores.js';
 
     function openNewFunction(functionId) {
