654467ff794fd09458d45671dc72fa3f6171d1a7
GameSquatch <broncoscorpio@gmail.com>
2022-07-25 11:27:02 -0600

    Update code generator and writer

--- code_generator/code_generator.js
+++ code_generator/code_generator.js
@@ -1,18 +1,21 @@
 const { codeWriter, mockData } = require('./code_writer');
 
-let variableInitBlock = "";
-for (let variable of Object.values(mockData.main.info.variables)) {
-    variableInitBlock += `let ${variable.name} = ${variable.returns === "String" ? '"' + variable.value + '"' : variable.value};\n`;
-}
-variableInitBlock += "\n";
+for (let fileId of Object.keys(mockData)) {
+    const currentFn = mockData[fileId];
+    let variableInitBlock = "";
+    for (let variable of Object.values(currentFn.main.info.variables)) {
+        variableInitBlock += `let ${variable.name} = ${variable.dataType === "String" ? '"' + variable.defaultValue + '"' : variable.defaultValue};\n`;
+    }
+    variableInitBlock += "\n";
 
-let codeResult = "";
-for (let statement of mockData.main.body) {
-    codeResult += codeOutput(statement);
+    let codeResult = "";
+    for (let statement of currentFn.main.body) {
+        codeResult += codeOutput(statement, fileId);
+    }
+    console.log(variableInitBlock + codeResult);
 }
-console.log(variableInitBlock + codeResult);
 
-function codeOutput(node) {
+function codeOutput(node, fileId) {
     // write code for node.type to a file, but for now just log it
-    return codeWriter[node.type](node);
+    return codeWriter[node.type](node, fileId);
 }
--- code_generator/code_writer.js
+++ code_generator/code_writer.js
@@ -1,134 +1,174 @@
 const uuidv4 = require('uuid').v4;
 
-const var1 = uuidv4();
-const var2 = uuidv4();
+
+const var1 = "4d6b745f-a0e2-4b09-8657-479edfcd79b0";//uuidv4();
+const var2 = "e7abc1fa-8630-46ef-8a31-5c4667c1be65";//uuidv4();
+const var3 = "88d2fc6d-3c58-4cef-8eda-d84617bb135f";//uuidv4();
 
 const mockData = {
-    "main": {
-        "info": {
-            "variables": {
-                [var1]: {
-                    "name": "aStr",
-                    "value": "",
-                    "returns": "String"
+    "abc": {
+        "main": {
+            "info": {
+                "id": "abc",
+                "variables": {
+                    [var1]: {
+                        "name": "aStr",
+                        "defaultValue": "",
+                        "dataType": "String"
+                    },
+                    [var2]: {
+                        "name": "aNum",
+                        "defaultValue": 0,
+                        "dataType": "Integer"
+                    }
                 },
-                [var2]: {
-                    "name": "aNum",
-                    "value": 0,
-                    "returns": "Integer"
-                }
+                "parameters": {},
             },
-            "parameters": [],
-        },
-        "body": [
-            {
-                "type": "ExpressionStatement",
-                "id": uuidv4(),
-                "expression": {
-                    "type": "AssignmentExpression",
-                    "left": {
-                        "type": "VarIdentifier",
-                        "refId": var1,
-                        "returns": "String"
-                    },
-                    "right": {
-                        "type": "UtilityCallExpression",
-                        "utilityName": "StringUtil",
-                        "utilityMethod": "concat",
-                        "arguments": [
-                            {
-                                "type": "StringLiteral",
-                                "value": "A long string teehee",
-                                "returns": "String"
-                            },
-                            {
-                                "type": "VarIdentifier",
-                                "refId": var1,
-                                "returns": "String"
-                            }
-                        ],
-                        "returns": "String"
-                    }
-                }
-            },
-            {
-                "type": "ExpressionStatement",
-                "id": uuidv4(),
-                "expression": {
-                    "type": "AssignmentExpression",
-                    "left": {
-                        "type": "VarIdentifier",
-                        "refId": var1,
-                        "returns": "String"
-                    },
-                    "right": {
-                        "type": "VarCallExpression",
-                        "variable": {
-                            "type": "VarIdentifier",
+            "body": [
+                {
+                    "type": "ExpressionStatement",
+                    "id": uuidv4(),
+                    "expression": {
+                        "type": "AssignmentExpression",
+                        "left": {
+                            "type": "RefIdentifier",
                             "refId": var1,
-                            "returns": "String"
+                            "dataType": "String",
+                            "fnRefType": "variables"
                         },
-                        "method": "concat",
-                        "arguments": [
-                            {
-                                "type": "StringLiteral",
-                                "value": "A long string teehee",
-                                "returns": "String"
-                            }
-                        ],
-                        "returns": "String"
+                        "right": {
+                            "type": "UtilityCallExpression",
+                            "utilityName": "StringUtil",
+                            "utilityMethod": "concat",
+                            "arguments": [
+                                {
+                                    "type": "StringLiteral",
+                                    "value": "A long string teehee",
+                                    "dataType": "String"
+                                },
+                                {
+                                    "type": "RefIdentifier",
+                                    "refId": var1,
+                                    "dataType": "String",
+                                    "fnRefType": "variables"
+                                }
+                            ],
+                            "dataType": "String"
+                        }
+                    }
+                },
+                {
+                    "type": "ExpressionStatement",
+                    "id": uuidv4(),
+                    "expression": {
+                        "type": "AssignmentExpression",
+                        "left": {
+                            "type": "RefIdentifier",
+                            "refId": var1,
+                            "dataType": "String",
+                            "fnRefType": "variables"
+                        },
+                        "right": {
+                            "type": "IdentifierRefCallExpression",
+                            "refData": {
+                                "type": "RefIdentifier",
+                                "refId": var1,
+                                "dataType": "String",
+                                "fnRefType": "variables"
+                            },
+                            "method": "concat",
+                            "arguments": [
+                                {
+                                    "type": "StringLiteral",
+                                    "value": "A long string teehee",
+                                    "dataType": "String"
+                                }
+                            ],
+                            "dataType": "String"
+                        }
+                    }
+                },
+                {
+                    "type": "ExpressionStatement",
+                    "id": uuidv4(),
+                    "expression": {
+                        "type": "AssignmentExpression",
+                        "left": {
+                            "type": "RefIdentifier",
+                            "refId": var2,
+                            "dataType": "Integer",
+                            "fnRefType": "variables"
+                        },
+                        "right": {
+                            "type": "UtilityCallExpression",
+                            "utilityName": "StringUtil",
+                            "utilityMethod": "length",
+                            "arguments": [
+                                {
+                                    "type": "StringLiteral",
+                                    "value": "Counting the length of the string",
+                                    "dataType": "String"
+                                }
+                            ],
+                            "dataType": "Integer"
+                        }
                     }
                 }
-            },
-            {
-                "type": "ExpressionStatement",
-                "id": uuidv4(),
-                "expression": {
-                    "type": "AssignmentExpression",
-                    "left": {
-                        "type": "VarIdentifier",
-                        "refId": var2,
-                        "returns": "Integer"
-                    },
-                    "right": {
-                        "type": "UtilityCallExpression",
-                        "utilityName": "StringUtil",
-                        "utilityMethod": "length",
-                        "arguments": [
-                            {
-                                "type": "StringLiteral",
-                                "value": "Counting the length of the string",
-                                "returns": "String"
-                            }
-                        ],
-                        "returns": "Integer"
+            ]
+        }
+    },
+    "123": {
+        "main": {
+            "info": {
+                "id": "123",
+                "variables": {
+                    [var3]: {
+                        "name": "Fn2Str",
+                        "defaultValue": "hello",
+                        "dataType": "String"
                     }
-                }
+                },
+                "parameters": {}
             },
-            {
-                "type": "ExpressionStatement",
-                "id": uuidv4(),
-                "expression": null
-            },
-            {
-                "type": "ExpressionStatement",
-                "id": uuidv4(),
-                "expression": null
-            },
-            {
-                "type": "ExpressionStatement",
-                "id": uuidv4(),
-                "expression": null
-            },
-            {
-                "type": "ExpressionStatement",
-                "id": uuidv4(),
-                "expression": null
-            },
-        ]
+            "body": [
+                {
+                    "type": "ExpressionStatement",
+                    "id": uuidv4(),
+                    "expression": {
+                        "type": "AssignmentExpression",
+                        "left": {
+                            "type": "RefIdentifier",
+                            "refId": var3,
+                            "dataType": "String",
+                            "fnRefType": "variables"
+                        },
+                        "right": null
+                    }
+                },
+            ]
+        }
     }
 };
 exports.mockData = mockData;
+
+const fm = {
+    "abc": {
+        title: "Main",
+        fileType: "function",
+        objectFlowData: {
+            parameters: {},
+            dataType: "String"
+        }
+    },
+    "123": {
+        title: "Fn2",
+        fileType: "function",
+        objectFlowData: {
+            parameters: {},
+            dataType: "String"
+        }
+    }
+};
 
 /**
  * @typedef {(ASTNode) => string} CodeGenerator
@@ -138,44 +178,53 @@ exports.mockData = mockData;
  * @typedef {Object.<string, CodeGenerator>} CodeWriterConfig
  */
 exports.codeWriter = {
-    ExpressionStatement: function(node) {
-        return node.expression === null ? "" : this[node.expression.type](node.expression) + ";\n";
+    ExpressionStatement: function(node, fileId) {
+        return node.expression === null ? "" : this[node.expression.type](node.expression, fileId) + ";\n";
     },
-    UtilityCallExpression: function(node) {
+    UtilityCallExpression: function(node, fileId) {
         const utility = `${node.utilityName}.${node.utilityMethod}`;
-        let arguments = node.arguments.reduce((acc, arg, i) => argStringBuilder.call(this, acc, arg, i), "");
+        let arguments = node.arguments.reduce((acc, arg, i) => argStringBuilder.call(this, acc, arg, i, fileId), "");
 
         return `${utility}(${arguments})`;
     },
-    VarCallExpression: function(node) {
-        const varCall = `${this.VarIdentifier(node.variable)}.${node.method}`;
-        let arguments = node.arguments.reduce((acc, arg, i) => argStringBuilder.call(this, acc, arg, i), "");
+    FunctionCallExpression: function(node, fileId) {
+        const title = fm[node.fileId].title;
+        let arguments = node.arguments.reduce((acc, arg, i) => argStringBuilder.call(this, acc, arg, i, fileId), "");
+
+        return `${title}.(${arguments})`;
+    },
+    IdentifierRefCallExpression: function(node, fileId) {
+        const varCall = `${this.RefIdentifier(node.refData, fileId)}.${node.method}`;
+        let arguments = node.arguments.reduce((acc, arg, i) => argStringBuilder.call(this, acc, arg, i, fileId), "");
 
         return `${varCall}(${arguments})`;
     },
-    AssignmentExpression: function(node) {
-        const left = this[node.left.type](node.left);
-        const right = this[node.right.type](node.right);
+    AssignmentExpression: function(node, fileId) {
+        const left = this[node.left.type](node.left, fileId);
+        if (node.right === null) return left;
+        const right = this[node.right.type](node.right, fileId);
 
         return `${left} = ${right}`;
     },
-    Identifier: function(node) {
-        return node.name;
-    },
-    VarIdentifier: function(node) {
-        const varRef = mockData.main.info.variables[node.refId];
+    RefIdentifier: function(node, fileId) {
+        let variableRef;
+        if (node.fnRefType === 'variables') {
+            variableRef = mockData[fileId].main.info.variables[node.refId];
+        } else {
+            variableRef = fm[fileId].objectFlowData.parameters[node.refId];
+        }
 
-        return varRef.name;
+        return variableRef.name;
     },
-    StringLiteral: function(node) {
+    StringLiteral: function(node, fileId) {
         return `"${node.value}"`;
     },
-    IntegerLiteral: function(node) {
+    IntegerLiteral: function(node, fileId) {
         return node.value.toString();
     }
 };
 
-function argStringBuilder(acc, arg, index) {
+function argStringBuilder(acc, arg, index, fileId) {
     //console.log(`inside reduce argtype: ${arg.type}`);
-    return acc += index > 0 ? `, ${this[arg.type](arg)}` : this[arg.type](arg);
+    return acc += index > 0 ? `, ${this[arg.type](arg, fileId)}` : this[arg.type](arg, fileId);
 }
