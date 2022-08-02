6338a8aa2b893c27b891cc528b439b83cc7a09f4
GameSquatch <broncoscorpio@gmail.com>
2022-08-01 19:50:08 -0600

    Add a logout button

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,11 +2,23 @@
     import { routes } from '../store/routes.js';
 
     const headerText = "Z-Flow";
+
+    function logout() {
+        fetch('/logout', {
+            method: 'POST'
+        })
+            .finally(() => {
+                window.location.reload();
+            });
+    }
 </script>
 
 <div class="header">
     <h1>{headerText}</h1>
-    <button on:click={(_) => $routes = '/run'}>Run</button>
+    <div class="flex">
+        <button on:click={(_) => $routes = '/run'}>Run</button>
+        <button on:click={(_) => logout()}>Log Out</button>
+    </div>
 </div>
 
 <style>
