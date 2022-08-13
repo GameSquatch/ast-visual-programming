5a48e4595779ea242fadc6774351b3d463d88988
GameSquatch <broncoscorpio@gmail.com>
2022-08-13 12:45:53 -0600

    Add var and param quick access

---
+++ src/components/QuickAccessDraggables.svelte
@@ -0,0 +1,95 @@
+<script>
+    import { fileMetadata } from './side_nav/file_metadata.js';
+    import { fnInfoRefObjectDrag } from '../lib/js/drag_and_drop/drag_start_data_creators.js';
+
+    export let functionInfo;
+
+    console.log(functionInfo);
+
+    $: metaData = $fileMetadata[functionInfo.id];
+
+    function dragStart(event, varData) {
+        const dragData = fnInfoRefObjectDrag(varData);
+        event.dataTransfer.setData('text/json', JSON.stringify(dragData));
+    }
+</script>
+
+<div class="quick-access-floater">
+    <p>Quick Access</p>
+    <div class="left-col">
+        <p class="col-header">Variables</p>
+        {#each Object.keys(functionInfo.variables) as variableId (variableId)}
+            {@const varData = functionInfo.variables[variableId]}
+            <span
+                on:dragstart={(event) =>
+                    dragStart(event, {
+                        ...varData,
+                        refId: variableId,
+                        dragType: 'variableRef',
+                        fnRefType: 'variables'
+                    })}
+                class="var-tag"
+                draggable="true"
+                >{varData.name}
+            </span>
+        {/each}
+    </div>
+
+    <div class="right-col">
+        <p class="col-header">Parameters</p>
+        {#each Object.keys(metaData.objectFlowData.parameters) as paramId (paramId)}
+            {@const paramData = metaData.objectFlowData.parameters[paramId]}
+            <span
+                on:dragstart={(event) =>
+                    dragStart(event, {
+                        ...paramData,
+                        refId: paramId,
+                        dragType: 'variableRef',
+                        fnRefType: 'parameters'
+                    })}
+                class="var-tag"
+                draggable="true">{paramData.name}</span>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .quick-access-floater {
+        z-index: 2;
+        position: absolute;
+        top: 25px;
+        right: 0;
+        height: 150px;
+        width: 30%;
+        min-width: 350px;
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .quick-access-floater > p {
+        width: 100%;
+        text-align: center;
+    }
+
+    .var-tag {
+        padding: 3px 12px;
+        border-radius: 20px;
+        background-color: #efefef;
+        cursor: move;
+        min-width: 80px;
+        text-align: center;
+        margin-bottom: 10px;
+        box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.24);
+    }
+
+    .left-col,
+    .right-col {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .col-header {
+        margin-bottom: 10px;
+    }
+</style>
--- src/components/tabbed_editor/TabView.svelte
+++ src/components/tabbed_editor/TabView.svelte
@@ -1,9 +1,11 @@
 <script>
-	import Flow from '../Flow.svelte';
-	import FunctionInfoTab from '../FunctionInfoTab.svelte';
+    import Flow from '../Flow.svelte';
+    import FunctionInfoTab from '../FunctionInfoTab.svelte';
+	import QuickAccessDraggables from '../QuickAccessDraggables.svelte';
 
-	export let tabViewData;
+    export let tabViewData;
 </script>
 
 <FunctionInfoTab bind:info={tabViewData.info} />
+<QuickAccessDraggables functionInfo={tabViewData.info} />
 <Flow flowData={tabViewData} />
