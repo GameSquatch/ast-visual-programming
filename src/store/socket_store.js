eb3745471250d16e994400f97fe8577a54beea07
GameSquatch <broncoscorpio@gmail.com>
2022-11-02 21:28:17 -0600

    Add comment

--- src/store/socket_store.js
+++ src/store/socket_store.js
@@ -2,6 +2,8 @@ import { writable, get } from 'svelte/store';
 import { io } from 'socket.io-client';
 
 
+// I don't know that this needs to be a store; can probably be a normal object since
+// it's not subscribed to like a store would be
 const { update, set, subscribe } = writable(null);
 
 const socketStore = {
