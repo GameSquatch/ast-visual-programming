c2cf6d85444246dac77db8161dcb271b7ad3c631
ewilliams-zoot <ezekiel.williams@zootweb.com>
2023-02-01 16:57:26 -0700

    Fix log in cookie domain name

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -2,7 +2,7 @@ import type { Actions } from "@sveltejs/kit";
 import { redirect } from "@sveltejs/kit";
 
 export const actions: Actions = {
-    default: async ({ cookies, request }) => {
+    default: async ({ cookies, request, url }) => {
         const authData = await request.formData();
         const username = authData.get('username');
         const password = authData.get('password');
@@ -10,7 +10,7 @@ export const actions: Actions = {
         if (username && username === 'bob' && password && password === 'bobsecret') {
             const now = new Date();
             cookies.set('authToken', 'abcdefg', {
-                domain: 'localhost',
+                domain: url.hostname,
                 path: '/',
                 httpOnly: true,
                 expires: new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1, now.getMinutes(), now.getSeconds()),
