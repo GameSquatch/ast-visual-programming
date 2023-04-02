18a8a956a87800e7b0743b67c4a1145122d37c53
ewilliams-zoot <ezekiel.williams@zootweb.com>
2023-04-02 09:54:09 -0600

    Starting a draggable component; to finish later

---
+++ src/lib/components/Draggable.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let onDragStart: (event: DragEvent) => void;
+    export let stopPropagation = false;
+
+    function dragStartHandler(event: DragEvent): void {
+        onDragStart(event);
+        if (stopPropagation) {
+            event.stopPropagation();
+        }
+    }
+</script>
+
+<div draggable="true" on:dragstart={dragStartHandler} style="display:inline-block">
+    <slot></slot>
+</div>
